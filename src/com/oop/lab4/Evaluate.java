41b2af0fabf7b2fa3eaec7e340c6d2c98e61257b
Isac Artur <isacartur@gmail.com>
2020-10-29 19:25:08 +0200

    Lab 4 + Lab 5 init

--- src/com/oop/lab4/Evaluate.java
+++ src/com/oop/lab4/Evaluate.java
@@ -5,93 +5,65 @@ import java.util.Stack;
 public class Evaluate {
     String expression;
 
-    public Evaluate(String _expression){
+    public Evaluate(String _expression) {
         expression = _expression;
     }
 
-    public int eval(){
-        {
-            char[] tokens = expression.toCharArray();
+    public int eval() {
+        char[] tokens = expression.toCharArray();
+        Stack<Integer> values = new
+                Stack<Integer>();
+        Stack<Character> ops = new
+                Stack<Character>();
 
-            // Stack for numbers: 'values'
-            Stack<Integer> values = new
-                    Stack<Integer>();
-
-            // Stack for Operators: 'ops'
-            Stack<Character> ops = new
-                    Stack<Character>();
-
-            for (int i = 0; i < tokens.length; i++)
-            {
-
-                // Current token is a
-                // whitespace, skip it
-                if (tokens[i] == ' ')
-                    continue;
-
-                // Current token is a number,
-                // push it to stack for numbers
-                if (tokens[i] >= '0' &&
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i] == ' ')
+                continue;
+            if (tokens[i] >= '0' &&
+                    tokens[i] <= '9') {
+                StringBuffer sbuf = new
+                        StringBuffer();
+                while (i < tokens.length &&
+                        tokens[i] >= '0' &&
                         tokens[i] <= '9')
-                {
-                    StringBuffer sbuf = new
-                            StringBuffer();
-
-                    // There may be more than one
-                    // digits in number
-                    while (i < tokens.length &&
-                            tokens[i] >= '0' &&
-                            tokens[i] <= '9')
-                        sbuf.append(tokens[i++]);
-                    values.push(Integer.parseInt(sbuf.
-                            toString()));
-
-                    i--;
-                }
-
-                else if (tokens[i] == '(')
-                    ops.push(tokens[i]);
-
-                else if (tokens[i] == ')')
-                {
-                    while (ops.peek() != '(')
-                        values.push(applyOp(ops.pop(),
-                                values.pop(),
-                                values.pop()));
-                    ops.pop();
-                }
-
-                // Current token is an operator.
-                else if (tokens[i] == '+' ||
-                        tokens[i] == '-' ||
-                        tokens[i] == '*' ||
-                        tokens[i] == '/')
-                {
-                    while (!ops.empty() &&
-                            hasPrecedence(tokens[i],
-                                    ops.peek()))
-                        values.push(applyOp(ops.pop(),
-                                values.pop(),
-                                values.pop()));
-
-                    // Push current token to 'ops'.
-                    ops.push(tokens[i]);
-                }
+                    sbuf.append(tokens[i++]);
+                values.push(Integer.parseInt(sbuf.
+                        toString()));
+                i--;
             }
-            while (!ops.empty())
-                values.push(applyOp(ops.pop(),
-                        values.pop(),
-                        values.pop()));
+            else if (tokens[i] == '(')
+                ops.push(tokens[i]);
+            else if (tokens[i] == ')') {
+                while (ops.peek() != '(')
+                    values.push(applyOp(ops.pop(),
+                            values.pop(),
+                            values.pop()));
+                ops.pop();
+            }
+            else if (tokens[i] == '+' ||
+                    tokens[i] == '-' ||
+                    tokens[i] == '*' ||
+                    tokens[i] == '/') {
+                while (!ops.empty() &&
+                        hasPrecedence(tokens[i],
+                                ops.peek()))
+                    values.push(applyOp(ops.pop(),
+                            values.pop(),
+                            values.pop()));
 
-            // Top of 'values' contains
-            // result, return it
-            return values.pop();
+                // Push current token to 'ops'.
+                ops.push(tokens[i]);
+            }
         }
-    }
+        while (!ops.empty())
+            values.push(applyOp(ops.pop(),
+                    values.pop(),
+                    values.pop()));
 
+        return values.pop();
+    }
     public static boolean hasPrecedence(
-            char op1, char op2)
-    {
+            char op1, char op2) {
         if (op2 == '(' || op2 == ')')
             return false;
         return (op1 != '*' && op1 != '/') ||
@@ -99,10 +71,8 @@ public class Evaluate {
     }
 
     public static int applyOp(char op,
-                              int b, int a)
-    {
-        switch (op)
-        {
+                              int b, int a) {
+        switch (op) {
             case '+':
                 return a + b;
             case '-':
--- src/com/oop/lab4/Main.java
+++ src/com/oop/lab4/Main.java
@@ -1,8 +1,25 @@
 package com.oop.lab4;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        Evaluate first = new Evaluate("2 * 4 + (3 +6 * 9 -    12 * (2*9 - 15))  + 2 +(-4)*((2 - 15))");
-        System.out.println(first.eval());
+    public static void main(String[] args) throws IOException {
+        String path = "src/static/lab4_1.txt";
+        File read_file = new File(path);
+        Scanner scan_file = new Scanner(read_file);
+
+        while(scan_file.hasNextLine()){
+            Checker check_line = new Checker(scan_file.nextLine());
+            if(!check_line.checkExpr()){
+                System.out.println("Incorrect expression");
+            }
+            else{
+
+                Evaluate eval_line = new Evaluate(check_line.getExpression());
+                System.out.println(eval_line.eval());
+            }
+        }
     }
 }
